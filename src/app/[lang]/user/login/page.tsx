ba9cfbc9e0deda9594c3fc4d61ea7f9b91799a4c
hy-rf <heyu880202@gmail.com>
2024-06-26 22:16:58 +0800

    Expose Backend url to public

--- src/app/[lang]/user/login/page.tsx
+++ src/app/[lang]/user/login/page.tsx
@@ -10,13 +10,16 @@ export default function Login({ params }: any) {
       <GoogleOAuthProvider clientId="496489862453-8ka1nrk1hi6m68vs37bl79utffnub1i7.apps.googleusercontent.com">
         <GoogleLogin
           onSuccess={async (tokenResponse) => {
-            const res = await axios(`${process.env.BACKEND_URL}/login`, {
-              method: "post",
-              headers: {
-                Authorization: tokenResponse.credential,
-              },
-              withCredentials: true,
-            });
+            const res = await axios(
+              `${process.env.NEXT_PUBLIC_BACKEND_URL}/login`,
+              {
+                method: "post",
+                headers: {
+                  Authorization: tokenResponse.credential,
+                },
+                withCredentials: true,
+              }
+            );
           }}
         ></GoogleLogin>
       </GoogleOAuthProvider>
