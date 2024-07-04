a8a3b5927f13a2daac3cd3bf4c4a1f1f43807117
hy-rf <heyu880202@gmail.com>
2024-07-04 13:11:56 +0800

    Add getme in user page

--- src/app/[lang]/user/page.tsx
+++ src/app/[lang]/user/page.tsx
@@ -1,4 +1,7 @@
+import axios from "axios";
 import type { Metadata, ResolvingMetadata } from "next";
+import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
 type Props = {
   params: {
     id: string;
@@ -13,7 +16,16 @@ export async function generateMetadata(
     description: "User",
   };
 }
-export default function User() {
+async function getMe() {
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user`, {
+    headers: {
+      Authorization: cookies().get("token")?.value || "",
+    },
+  });
+  console.log(res);
+}
+export default function User(req: NextRequest) {
+  getMe();
   return (
     <>
       <h3 className="text-3xl font-bold underline">User</h3>
