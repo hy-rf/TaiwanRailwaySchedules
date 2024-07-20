63d720df65381db46b8f9164c27ef728e924946b
hy-rf <heyu880202@gmail.com>
2024-07-20 13:28:49 +0800

    Add line client component

--- src/app/[lang]/rail/page.tsx
+++ src/app/[lang]/rail/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LineInfoClient from "@/components/rail/LineInfoClient";
 import NearStation from "@/components/rail/NearStation";
 import TimeBoard from "@/type/rail/TimeBoard";
 import { useEffect, useState } from "react";
@@ -17,7 +18,9 @@ export default function Page() {
 
   return (
     <div className="p-1">
-      <NearStation />
+      {/* <NearStation /> */}
+      <LineInfoClient />
+      <p></p>
     </div>
   );
 }
