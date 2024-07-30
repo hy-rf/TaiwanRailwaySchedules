20c9a0c4c15bbe5cc71d9063cbe8b927c56d137d
hy-rf <heyu880202@gmail.com>
2024-07-30 17:35:18 +0800

    style: change background color of links in rail layout

--- src/app/[lang]/rail/layout.tsx
+++ src/app/[lang]/rail/layout.tsx
@@ -11,39 +11,26 @@ export default async function RootLayout({
 }) {
   const dict = await getDictionary(params.lang);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-      }}
-    >
-      <div
-        className="p-1"
-        style={{
-          position: "static",
-          left: 0,
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <a
-          href="/rail/line"
-          style={{
-            color: "blue",
-            textDecoration: "underline",
-          }}
-        >
-          {dict.main.rail.nav.linetitle}
-        </a>
-        <a
-          href="/rail/station"
-          style={{
-            color: "blue",
-            textDecoration: "underline",
-          }}
-        >
-          {dict.main.rail.nav.stationtitle}
-        </a>
+    <div className="flex">
+      <div className="p-1 static bg-slate-200">
+        <ul className="font-medium">
+          <li>
+            <a
+              href="/rail/line"
+              className="antialiased block py-2 px-3 text-gray-800 rounded bg-slate-100 aria-checked:bg-sky-700"
+            >
+              {dict.main.rail.nav.linetitle}
+            </a>
+          </li>
+          <li>
+            <a
+              href="/rail/station"
+              className="antialiased block py-2 px-3 text-gray-800 rounded bg-slate-100 aria-checked:bg-sky-700"
+            >
+              {dict.main.rail.nav.stationtitle}
+            </a>
+          </li>
+        </ul>
       </div>
       <div>{children}</div>
     </div>
